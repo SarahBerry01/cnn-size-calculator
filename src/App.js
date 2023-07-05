19bc4d76aa3412abd3d3c1577804ae3285b66e04
SarahBerry01 <sarah@berrys.me.uk>
2023-07-05 09:22:43 +0100

    fix formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import SizeInputs from './SizeInputs';
 import ConvLayers from './ConvLayers'
-import Test_f from './ConvLayers'
 import './App.css';
 
 function App() {
   return (
     <div>
-    <div><ConvLayers/></div>
+    <ConvLayers/>
     </div>
   )
 }
--- src/ConvLayers.js
+++ src/ConvLayers.js
@@ -1,5 +1,36 @@
 import React, { useState } from 'react';
 
+function Table(props) {
+    return (
+        <div className='box-container'>
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Channels Out</th>
+                        <th>Kernel</th>
+                        <th>Stride</th>
+                        <th>Padding</th>
+                        <th>Calc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.data.map((row, index) => (
+                        <tr key={index}>
+                            <td>{row.channels}</td>
+                            <td>{row.kernel}</td>
+                            <td>{row.stride}</td>
+                            <td>{row.padding}</td>
+                            <td>{row.padding * row.channels}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>);
+
+}
+
+
 function ConvLayers() {
     const [data, setData] = useState([]);
     const [inputs, setInputs] = useState({
@@ -15,62 +46,40 @@ function ConvLayers() {
         const newRow = { ...inputs };
         setData([...data, newRow]);
         setInputs({ channels: '', kernel: '', stride: '', padding: '' }); // Clear input values after submission
+        console.log(data);
     };
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div class="grid-container">
-                    <div class="grid-item">
-                        <label>
-                            Channels:
-                            <input type="text" name="channels" value={inputs.channels} onChange={handleInputChange} />
-                        </label>
-                    </div>
-                    <div class="grid-item">
-                        <label>
-                            Kernel:
-                            <input type="text" name="kernel" value={inputs.kernel} onChange={handleInputChange} />
-                        </label>
-                    </div>
-                    <div class="grid-item">
-                        <label>
-                            Stride:
-                            <input type="text" name="stride" value={inputs.stride} onChange={handleInputChange} />
-                        </label>
-                    </div>
-                    <div class="grid-item">
-                        <label>
-                            Padding:
-                            <input type="text" name="padding" value={inputs.padding} onChange={handleInputChange} />
-                        </label>
-                    </div>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            <div className='box-container'>
+                <h3>Add Convolution Layer</h3>
+                <form onSubmit={handleSubmit}>
+                    <div class="grid-container">
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Channels Out</th>
-                        <th>Kernel</th>
-                        <th>Stride</th>
-                        <th>Padding</th>
-                        <th>Calc</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((row, index) => (
-                        <tr key={index}>
-                            <td>{row.channels}</td>
-                            <td>{row.kernel}</td>
-                            <td>{row.stride}</td>
-                            <td>{row.padding}</td>
-                            <td>{row.padding * row.channels}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                        <div class="grid-item">
+                            <label>Channels<br /></label>
+
+                            <input type="number" name="channels" value={inputs.channels} onChange={handleInputChange} />
+                        </div>
+                        <div class="grid-item">
+                            <label>Kernel<br /></label>
+
+                            <input type="number" name="kernel" value={inputs.kernel} onChange={handleInputChange} />
+                        </div>
+                        <div class="grid-item">
+                            <label>Stride<br /></label>
+                            <input type="number" name="stride" value={inputs.stride} onChange={handleInputChange} />
+                        </div>
+                        <div class="grid-item">
+                            <label>Padding<br /></label>
+                            <input type="number" name="padding" value={inputs.padding} onChange={handleInputChange} />
+                        </div>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <Table data={data}/>
+
         </div>
     );
 }
